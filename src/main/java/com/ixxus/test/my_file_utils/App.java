77ff3afc5387446398e1455e840077fb1433f162
Nazareth <nazareth@Nazareths-MacBook-Pro.local>
2016-01-19 10:03:27 +0100

    First version

---
+++ src/main/java/com/ixxus/test/my_file_utils/App.java
@@ -0,0 +1,13 @@
+package com.ixxus.test.my_file_utils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
